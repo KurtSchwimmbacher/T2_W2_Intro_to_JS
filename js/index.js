c45ed2535508b529120d9dc81f3a1e26d60f57f9
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-05-04 20:43:55 +0200

    add js
    add js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+
+var firstName = "Kurt";
+let surname ="Schwimmbacher";
+const subject = "Development";
+
+console.log(firstName);
+console.log(surname);
+console.log(subject);
